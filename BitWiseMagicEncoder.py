473768b9c2dc3bb65b35ce6e05c40105f92546d3
Kuba Siekierzyński <jsieki2@gmail.com>
2017-10-28 22:31:23 +0200

    BitWise Magic Encoder - do your own magic!

---
+++ BitWiseMagicEncoder.py
@@ -0,0 +1,43 @@
+"""
+Bitwise magic encoder v. 1.0
+Coded by Kuba Siekierzyński (c) 2017
+Original code:
+https://code.sololearn.com/cr8UCuC08YuL/#py
+
+This code converts the k array into decimal row representations. The output numbers may be copied and later used in the "decoding machine" present in my other code:
+
+[Python] Bitwise Hello World:
+https://code.sololearn.com/cevCloev897h/#py
+
+Feel free to alter the array for your personal use.
+
+One remark - rows can be of different length, just remember when deciphering, to change the value in the row iterator to the maximum length.
+
+"""
+
+k = [
+[1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+[1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+[1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+[1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+[1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0],
+[1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+[1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+[1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+[0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0]]
+
+# this is just to check whether the output is what you wanted
+for j in k:
+    for i in j:
+        print(chr(176+43*i), end='')
+    print()
+
+print()
+# this is the actual encoding mechanism
+for j in k:
+    l = ''
+    for i in j:
+        l += str(i)
+    print(int(l, base=2))
